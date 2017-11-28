c38aae81cfc332a13e099136bda3cecf11055210
eldan1 <el0@mail.ru>
2017-11-28 08:23:38 +0400

    добавлены комментарии

--- Program.cs
+++ Program.cs
@@ -17,11 +17,10 @@ namespace EveryDay.BuhHelper
             Console.Write("Введите ИНН ");
             string inn = Console.ReadLine();
             Console.Write(inn);
+            // инн вводим и выврдим на консоль, но так как другие ИНН выдают "запрет" - возвращаем действуюущий
             inn = "6663003127";
             Console.Write(" ");
             Console.Write("всё равно используем 6663003127 так как другие запрещены");
-            //var url = "https://focus-api.kontur.ru/api3/req?key=3208d29d15c507395db770d0e65f3711e40374df&inn=6663003127";
-
             var url = "https://focus-api.kontur.ru/api3/req?key=3208d29d15c507395db770d0e65f3711e40374df&inn=" + inn;
             // Create a request for the URL. 
             WebRequest request = WebRequest.Create(url);
@@ -42,10 +41,13 @@ namespace EveryDay.BuhHelper
             
 
             Console.Write("Руководитель: ");
+            //выводим построку от heads до innfl, лишние вимволы обрезаем
             string s1 = responseFromServer.Substring(responseFromServer.IndexOf("heads")+15, responseFromServer.IndexOf("innfl") - responseFromServer.IndexOf("heads")-16);
             Console.WriteLine(s1);
 
+
             Console.Write("наименование: ");
+            //выводим построку от full до date, лишние cимволы обрезаем
             s1 = responseFromServer.Substring(responseFromServer.IndexOf("full")+6, responseFromServer.IndexOf("date")- responseFromServer.IndexOf("full")-10);
             Console.WriteLine(s1);
             // Clean up the streams and the response.
